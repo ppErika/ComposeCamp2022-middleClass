53994adfa4f4033622706c7ba8ef7b8181e0662b
Erika_Han <hanerika11@gmail.com>
2023-02-02 22:00:23 +0900

    04 Jetpack Compose 기초

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.codelab.theming.ui.start.theme
+
+class Color {
+    val Red700 = Color(0xffdd0d3c)
+    val Red800 = Color(0xffd00036)
+    val Red900 = Color(0xffc20029)
+}
---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,11 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+class Theme {
+}
+
+fun JetnewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
